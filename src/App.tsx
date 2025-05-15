bfa0d5e05e24d94da0681bf2ec65207d69dad2f1
Leonel Eisinger <42424255+leonelEisinger@users.noreply.github.com>
2025-05-15 09:28:09 -0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { PageHeader } from "./layouts/PageHeader"
-
 function App() {
 
   return (
     <>
-      <div className='max-h-screen flex flex-col'>
-        <PageHeader />
-        <div>2</div>
-      </div>
+      
     </>
   )
 }
